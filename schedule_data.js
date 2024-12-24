e8473c48de563c5e4970f1f2ffec8522e32c9073
Mr-Sheesh <46701750+Mr-Sheesh@users.noreply.github.com>
2024-12-25 03:26:51 +0900

    Add files via upload

--- schedule_data.js
+++ schedule_data.js
@@ -1,268 +1,268 @@
 // schedule_data.js
 const scheduleData = [
   {
-    "name": "",
-    "url": "",
+    "name": "授業名",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
-    "visible": false
-  },
-  {
-    "name": "",
-    "url": "",
-    "visible": false
-  },
-  {
-    "name": "",
-    "url": "",
-    "visible": false
-  },
-  {
-    "name": "",
-    "url": "",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
-    "visible": false
-  },
-  {
-    "name": "キリスト教概論Ⅱ",
-    "url": "https://cp.aim.aoyama.ac.jp/",
-    "visible": true
-  },
-  {
-    "name": "特殊講義Ｃ（Ⅲ）",
-    "url": "https://cp.aim.aoyama.ac.jp/",
-    "visible": true
-  },
-  {
-    "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
-    "visible": false
-  },
-  {
-    "name": "",
-    "url": "",
-    "visible": false
-  },
-  {
-    "name": "",
-    "url": "",
-    "visible": false
-  },
-  {
-    "name": "",
-    "url": "",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
-    "visible": false
-  },
-  {
-    "name": "統計学入門",
-    "url": "https://classroom.google.com/w/NTA5MDYxMzI3MzA3/t/all",
-    "visible": true
-  },
-  {
-    "name": "ジャーナリスト論",
-    "url": "https://cp.aim.aoyama.ac.jp/",
-    "visible": true
-  },
-  {
-    "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
-    "visible": false
-  },
-  {
-    "name": "",
-    "url": "",
-    "visible": false
-  },
-  {
-    "name": "応用空間情報学Ⅱ",
-    "url": "https://cp.aim.aoyama.ac.jp/",
-    "visible": true
-  },
-  {
-    "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
-    "visible": false
-  },
-  {
-    "name": "演習ⅠＢ",
-    "url": "https://cp.aim.aoyama.ac.jp/",
-    "visible": true
-  },
-  {
-    "name": "現代日本の論点",
-    "url": "https://cp.aim.aoyama.ac.jp/",
-    "visible": true
-  },
-  {
-    "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
-    "visible": false
-  },
-  {
-    "name": "",
-    "url": "",
-    "visible": false
-  },
-  {
-    "name": "応用空間情報学Ⅱ",
-    "url": "https://cp.aim.aoyama.ac.jp/",
-    "visible": true
-  },
-  {
-    "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
-    "visible": false
-  },
-  {
-    "name": "Lifestyle and Society in Japan",
-    "url": "https://cp.aim.aoyama.ac.jp/",
-    "visible": true
-  },
-  {
-    "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
-    "visible": false
-  },
-  {
-    "name": "",
-    "url": "",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
-    "visible": false
-  },
-  {
-    "name": "",
-    "url": "",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
     "visible": false
   },
   {
     "name": "授業名",
-    "url": "file:///C:/Users/sekis/Downloads/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8/%E6%99%82%E9%96%93%E5%89%B2%E8%A1%A8%E5%80%8B%E4%BA%BA%E7%94%A8/%E3%81%AA%E3%81%97",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
+    "visible": false
+  },
+  {
+    "name": "授業名",
+    "url": "なし",
     "visible": false
   }
 ];
